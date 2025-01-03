e1a72ed2fab5d2383fd4681c8eaff5d95fcd79af
helpinghand90 <benelmslie@gmail.com>
2025-01-03 07:28:49 +0000

    update name and database

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,14 @@
 import reflex as rx
-from decouple import config 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 DATABASE_URL = config("DATABASE_URL")
 
 config = rx.Config(
     app_name="reflex_gpt",
-    db_url=DATABASE_URL
+    db_url=os.getenv("DATABASE_URL"),
 )
