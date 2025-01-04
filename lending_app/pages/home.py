28cf37a0f13b88f46fb34f8fd8ed44f1ac5fe6b5
helpinghand90 <benelmslie@gmail.com>
2025-01-04 02:50:08 +0000

    home added section placeholder

--- lending_app/pages/home.py
+++ lending_app/pages/home.py
@@ -30,6 +30,22 @@ def home_page() -> rx.Component:
                 rx.box(width="10%"),
                 width="100%",
             ),
+            rx.box(height="5vh"),
+            rx.divider(),
+            rx.box(height="5vh"),
+            rx.hstack(
+                rx.box(
+                    rx.heading("Welcome to Part 2!", size="9", text_align="center"),
+                    rx.text(
+                        "lets keep going",
+                        size="5",
+                        text_align="center",
+                    ),
+                    width="100%",
+                    text_align="center",
+                ),
+                width="100%",
+            ),
             spacing="5",
             justify="center",
             min_height="85vh",
